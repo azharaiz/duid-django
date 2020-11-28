49232d6e288888e6a355e6105fe4c814636ab2bc
Azhar Rais <dev@azharaiz.com>
2020-11-28 11:48:34 +0700

    Init Django Rest Framework project

---
+++ duid/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+]
