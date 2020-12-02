21e5bd6bdcb3272670b78620e087193286efb17c
muhammad.feril <muhammad.feril@ui.ac.id>
2020-12-02 08:05:55 +0700

    [GREEN] add category model

--- category/models.py
+++ category/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from uuid import uuid4
 
-# Create your models here.
+TYPE_CHOICES = [
+    ("INCOME", "INCOME"),
+    ("EXPENSE", "EXPENSE")
+]
+
+class Category(models.Model):
+    category_id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    category_title = models.CharField(max_length=255)
+    category_type = models.CharField(max_length=10, choices=TYPE_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
