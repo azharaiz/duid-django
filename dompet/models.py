e785fd39fa9f7309e74d31a43e4caa65735a2472
Kevin Raikhan Zain <kevinraikhan@gmail.com>
2020-12-04 02:52:37 +0700

    [GREEN] Implemented model with user

--- dompet/models.py
+++ dompet/models.py
@@ -3,8 +3,12 @@ import uuid
 
 
 # Create your models here.
+from authentication.models import User
+
+
 class Dompet(models.Model):
     account_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     account_title = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
