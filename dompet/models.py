8319a72f31e32b4bb0cf01f5d2d008906aea91ce
Kevin Raikhan Zain <kevinraikhan@gmail.com>
2020-12-03 23:02:46 +0700

    [GREEN] Added dompet model no auth

--- dompet/models.py
+++ dompet/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+import uuid
 
 
+# Create your models here.
+class Dompet(models.Model):
+    account_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    account_title = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
