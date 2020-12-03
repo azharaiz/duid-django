c07eb468055c03d82541f78f7f969b783f12c2e7
Kevin Raikhan Zain <kevinraikhan@gmail.com>
2020-12-03 23:54:47 +0700

    [RED] Added test for dompet api/dompet

--- dompet/tests.py
+++ dompet/tests.py
@@ -1,10 +1,13 @@
+import json
 from datetime import datetime
 from unittest import mock
 
 import pytz
-from django.test import TestCase
+from django.test import TestCase, Client
 
 from dompet.models import Dompet
+
+ALL_DOMPET = '/api/dompet/'
 
 
 class DompetModelTest(TestCase):
@@ -51,3 +54,118 @@ class DompetModelTest(TestCase):
         self.assertEqual(len(Dompet.objects.all()), 2)
         Dompet.objects.filter(account_title=self.mock_account_title_1).delete()
         self.assertEqual(len(Dompet.objects.all()), 1)
+
+
+class DompetAPITest(TestCase):
+    def setUp(self):
+        self.new_dompet_post = Client().post(ALL_DOMPET, {"account_title": "test 0"})
+        self.newly_created_dompet = json.loads(self.new_dompet_post.content.decode('utf-8'))
+
+    # Test endpoint
+    def test_get_all_dompet_should_return_200(self):
+        response = Client().get(ALL_DOMPET)
+        self.assertEqual(response.status_code, 200)
+
+    def test_dompet_count_should_be_correct(self):
+        for i in range(1, 11):
+            response = Client().get(ALL_DOMPET)
+            json_response = json.loads(response.content.decode('utf-8'))
+            self.assertEqual(i, json_response['count'])
+            if i <= 10:
+                self.assertEqual(i, len(json_response['results']))
+            Dompet(account_title=f'testing {i}').save()
+
+    def test_pagination_should_be_correct(self):
+        response = Client().get(ALL_DOMPET + '?page=2')
+        self.assertEqual(response.status_code, 404)
+
+        response = Client().get(ALL_DOMPET)
+        json_response = json.loads(response.content.decode('utf-8'))
+        self.assertIsNone(json_response['next'])
+        self.assertIsNone(json_response['previous'])
+        for i in range(25):
+            Dompet(account_title=f'test {i}').save()
+
+        response = Client().get(ALL_DOMPET)
+        json_response = json.loads(response.content.decode('utf-8'))
+        self.assertIsNotNone(json_response['next'])
+        self.assertIsNone(json_response['previous'])
+
+        response = Client().get(ALL_DOMPET + '?page=2')
+        json_response = json.loads(response.content.decode('utf-8'))
+        self.assertIsNotNone(json_response['next'])
+        self.assertIsNotNone(json_response['previous'])
+
+        response = Client().get(ALL_DOMPET + '?page=3')
+        json_response = json.loads(response.content.decode('utf-8'))
+        self.assertIsNone(json_response['next'])
+        self.assertIsNotNone(json_response['previous'])
+
+    def test_get_all_dompet_item_name_correct(self):
+        for i in range(1, 10):
+            Dompet(account_title=f'test {i}').save()
+        response = Client().get(ALL_DOMPET)
+        json_response = json.loads(response.content.decode('utf-8'))
+        for i in range(len(json_response['results'])):
+            self.assertEqual(json_response['results'][i]['account_title'], f'test {i}')
+
+    def test_get_one_dompet(self):
+        new_dompet_response = Client().get(f'{ALL_DOMPET}{self.newly_created_dompet["account_id"]}/')
+        json_response = json.loads(new_dompet_response.content.decode('utf-8'))
+        self.assertEqual(self.newly_created_dompet, json_response)
+
+        # Negative test, should return not found
+        new_dompet_response = Client().get(f'{ALL_DOMPET}randomrandom/')
+        json_response = json.loads(new_dompet_response.content.decode('utf-8'))
+        self.assertEqual({"detail": "Not found."}, json_response)
+
+    def test_create_dompet_by_posting(self):
+        self.assertEqual(self.newly_created_dompet['account_title'], 'test 0')
+        self.assertEqual(Dompet.objects.all()[0].account_title, 'test 0')
+
+        # Negative test, account_title should not be empty
+        response = Client().post(ALL_DOMPET, {"account_title": ""})
+        json_response = json.loads(response.content.decode('utf-8'))
+        self.assertEqual(len(Dompet.objects.all()), 1)
+        self.assertEqual(json_response['account_title'][0], "This field may not be blank.")
+
+    def test_delete_dompet(self):
+        self.assertEqual(Dompet.objects.all()[0].account_title, self.newly_created_dompet['account_title'])
+
+        Client().delete(f'{ALL_DOMPET}{self.newly_created_dompet["account_id"]}/')
+
+        self.assertEqual(len(Dompet.objects.all()), 0)
+
+        response = Client().delete(f'{ALL_DOMPET}{self.newly_created_dompet["account_id"]}/')
+        del_response_json = json.loads(response.content.decode('utf-8'))
+        self.assertEqual({"detail": "Not found."}, del_response_json)
+
+    def test_update_dompet(self):
+        response = Client().get(f'{ALL_DOMPET}{self.newly_created_dompet["account_id"]}/')
+        json_response = json.loads(response.content.decode('utf-8'))
+        self.assertEqual(json_response, self.newly_created_dompet)
+
+        change_title = 'changed'
+
+        response = Client().put(f'{ALL_DOMPET}{self.newly_created_dompet["account_id"]}/', {
+            "account_id": "c6a1af05-ef26-416b-a4b1-e23b4942f50b",
+            "account_title": change_title,
+            "created_at": "2020-12-01T14:00:52.294269Z",
+            "updated_at": "2020-12-01T17:31:04.167919Z"
+        }, content_type='application/json')
+
+        json_response = json.loads(response.content.decode('utf-8'))
+        self.assertNotEqual(json_response['account_title'], self.newly_created_dompet['account_title'])
+        self.newly_created_dompet['account_title'] = change_title
+        self.assertEqual(json_response['account_title'], self.newly_created_dompet['account_title'])
+        self.assertEqual(json_response['account_id'], self.newly_created_dompet['account_id'])
+
+        # Negative test
+        response = Client().put(f'{ALL_DOMPET}randomm/', {
+            "account_id": "c6a1af05-ef26-416b-a4b1-e23b4942f50b",
+            "account_title": change_title,
+            "created_at": "2020-12-01T14:00:52.294269Z",
+            "updated_at": "2020-12-01T17:31:04.167919Z"
+        }, content_type='application/json')
+        json_response = json.loads(response.content.decode('utf-8'))
+        self.assertEqual(json_response, {"detail": "Not found."})
