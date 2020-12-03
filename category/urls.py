abe82525e03c8cddd9a930137d268f0386dd0ce9
muhammad.feril <muhammad.feril@ui.ac.id>
2020-12-04 06:27:24 +0700

    [GREEN] add get and delete in category item view

--- category/urls.py
+++ category/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CategoryListView
+from .views import CategoryListView, CategoryItemView
 
 app_name = 'category'
 urlpatterns = [
     path('all/', CategoryListView.as_view()),
+    path('item/', CategoryItemView.as_view())
 ]
--- category/views.py
+++ category/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist, ValidationError
+from rest_framework.exceptions import NotFound
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Category
 from .CategorySerializers import CategorySerializer
+from authentication.models import User
+import uuid
 
 class CategoryListView(APIView):
     permission_classes = [IsAuthenticated]
@@ -15,3 +19,28 @@ class CategoryListView(APIView):
         }
         return(Response(content))
 
+class CategoryItemView(APIView):
+    permission_classes = [IsAuthenticated]
+    def get(self, request):
+        category_id = request.data.get('category_id')
+        try:
+            category_item = Category.objects.get(category_id=category_id, user=request.user)
+        except(ObjectDoesNotExist, ValidationError):
+            raise NotFound(detail="data not found", code=404)
+
+        category_item_serializer = CategorySerializer(category_item, many=False)
+
+        return(Response(category_item_serializer.data))
+        
+    def delete(self, request):
+        category_id = request.data.get('category_id')
+        try:
+            category_item = Category.objects.get(category_id=category_id, user=request.user)
+        except(ObjectDoesNotExist, ValidationError):
+            raise NotFound(detail="data not found", code=404)
+
+        category_item.delete()
+        content = {
+                'message' : "success delete category"
+        }
+        return(Response(content))
