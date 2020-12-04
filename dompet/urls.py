df0c1320c74a8bdf018aa62ed29cf64fb43f82f2
Kevin Raikhan Zain <kevinraikhan@gmail.com>
2020-12-04 17:08:56 +0700

    [GREEN] Implemented user auth in api/dompet

--- dompet/urls.py
+++ dompet/urls.py
@@ -1,11 +1,12 @@
-from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
+
 from . import views
 
 router = routers.DefaultRouter()
-router.register('', views.DompetView)
+router.register('', views.DompetView, basename='dompet')
 
 urlpatterns = [
     path('', include(router.urls)),
+
 ]
