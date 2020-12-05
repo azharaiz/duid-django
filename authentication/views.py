06fc0cba40c7fd4c587375018918595b1f5fb90b
Azhar Rais <dev@azharaiz.com>
2020-12-05 15:39:15 +0700

    [GREEN] Implement update profile data

--- authentication/views.py
+++ authentication/views.py
@@ -13,3 +13,13 @@ class ProfileView(APIView):
         user = User.objects.get(id=request.user.id)
         user_serialized = UserSerializer(user)
         return Response(user_serialized.data)
+
+    def put(self, request):
+        user = User.objects.get(id=request.user.id)
+        user_data = UserSerializer(instance=user, data=request.data)
+
+        if not user_data.is_valid():
+            return Response({'message': 'Invalid data'}, status=400)
+
+        user_data.save()
+        return Response(user_data.data)
