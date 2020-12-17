6c17f64255621e70782045720ab3938a78896dd9
Kevin Raikhan Zain <kevinraikhan@gmail.com>
2020-12-17 15:23:44 +0700

    [GREEN] Added target due_date implementation

--- target/models.py
+++ target/models.py
@@ -2,13 +2,15 @@ import uuid
 
 from django.db import models
 
+from authentication.models import User
+
 
 # Create your models here.
-from authentication.models import User
 
 
 class Target(models.Model):
     target_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    due_date = models.DateField()
     target_title = models.CharField(max_length=100)
     target_amount = models.PositiveBigIntegerField()
