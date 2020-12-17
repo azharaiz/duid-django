36d69fe5b4bcff808634c61badc8cde5e33d06cf
Kevin Raikhan Zain <kevinraikhan@gmail.com>
2020-12-17 12:20:27 +0700

    [GREEN] Implemented target model

--- target/models.py
+++ target/models.py
@@ -1,1 +1,14 @@
+import uuid
+
+from django.db import models
+
+
 # Create your models here.
+from authentication.models import User
+
+
+class Target(models.Model):
+    target_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    target_title = models.CharField(max_length=100)
+    target_amount = models.PositiveBigIntegerField()
