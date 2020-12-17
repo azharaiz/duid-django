7700ee3a7458c56f58fa4abbab9872cea9b7fc33
Kevin Raikhan Zain <kevinraikhan@gmail.com>
2020-12-17 21:49:19 +0700

    [GREEN] Implemented target CRUD API

--- target/urls.py
+++ target/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
+from . import views
+
+router = routers.DefaultRouter()
+router.register('', views.TargetView, basename='target')
 
 urlpatterns = [
+    path('', include(router.urls)),
 
 ]
--- target/views.py
+++ target/views.py
@@ -1,1 +1,29 @@
-# Create your views here.
+from rest_framework import permissions, viewsets
+from rest_framework.exceptions import PermissionDenied
+
+from target.models import Target
+from target.serializers import TargetSerializer
+
+
+class IsOwner(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.user == request.user
+
+
+class TargetView(viewsets.ModelViewSet):
+    permission_classes = (IsOwner,)
+    serializer_class = TargetSerializer
+
+    # Ensure a user sees only own Dompet objects.
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_authenticated:
+            return Target.objects.filter(user=user)
+        raise PermissionDenied()
+
+    # Set user as owner of a Dompet object.
+    def perform_create(self, serializer):
+        try:
+            serializer.save(user=self.request.user)
+        except ValueError:
+            raise PermissionDenied()
