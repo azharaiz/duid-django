d5cac849fb97d94b39b1462d23b4191eb7728601
Azhar Rais <dev@azharaiz.com>
2020-12-18 11:28:00 +0700

    [INTEGRATION] Test User & Category module (Create, Get All)

--- tests/test_integrations.py
+++ tests/test_integrations.py
@@ -5,6 +5,7 @@ from rest_framework.test import APIClient
 
 URL_USER = '/api/user/'
 URL_PROFILE = '/api/auth/profile/'
+URL_CATEGORY = "/api/category/"
 MOCK_EMAIL = 'email@email.com'
 MOCK_PASSWORD = 'password'
 
@@ -34,3 +35,41 @@ class IntegrationTest(TestCase):
         response = self.client.get(URL_PROFILE, format='json')
         content = json.loads(response.content)
         self.assertEqual(MOCK_EMAIL, content.get('email'))
+
+    def test_create_user_then_create_category_finally_get_all_category_data(self):
+        response = self.client.post(URL_USER,
+                                    {'email': MOCK_EMAIL, 'password': MOCK_PASSWORD},
+                                    format='json')
+        self.assertEqual(response.status_code, 200)
+
+        user_id = json.loads(response.content).get('id')
+
+        response_token = self.client.post(
+            '/api/auth/token/',
+            {
+                'email': MOCK_EMAIL,
+                'password': MOCK_PASSWORD
+            },
+            format='json'
+        )
+
+        jwt_token = json.loads(response_token.content).get('access')
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + jwt_token)
+
+        category_data = {
+            "category_title": "Test Category",
+            "category_type": "INCOME"
+        }
+
+        response_create_category = self.client.post(URL_CATEGORY, category_data)
+
+        content = json.loads(response_create_category.content)
+        self.assertEqual(content.get('message'), 'success add category')
+
+        response_get_all_category = self.client.get(URL_CATEGORY)
+
+        content = json.loads(response_get_all_category.content)
+        self.assertTrue(content.get('category_list'))
+        self.assertEqual(category_data['category_title'], content.get('category_list')[0]['category_title'])
+        self.assertEqual(category_data['category_type'], content.get('category_list')[0]['category_type'])
+        self.assertEqual(user_id, content.get('category_list')[0]['user'])
