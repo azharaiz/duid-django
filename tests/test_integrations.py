a0fd640746375c55a311b7c3b91ef280e6646ee3
Azhar Rais <dev@azharaiz.com>
2020-12-18 16:25:27 +0700

    [INTEGRATION] Test user-auth-dompet

--- tests/test_integrations.py
+++ tests/test_integrations.py
@@ -3,9 +3,12 @@ import json
 from django.test import TestCase
 from rest_framework.test import APIClient
 
+from dompet.models import Dompet
+
 URL_USER = '/api/user/'
 URL_PROFILE = '/api/auth/profile/'
-URL_CATEGORY = "/api/category/"
+URL_CATEGORY = '/api/category/'
+URL_DOMPET = '/api/dompet/'
 MOCK_EMAIL = 'email@email.com'
 MOCK_PASSWORD = 'password'
 
@@ -114,3 +117,81 @@ class IntegrationTest(TestCase):
         content = json.loads(response_delete_category.content)
 
         self.assertEqual(content.get('message'), 'success delete category')
+
+    def test_user_then_auth_then_dompet(self):
+        response = self.client.post(URL_USER,
+                                    {'email': MOCK_EMAIL, 'password': MOCK_PASSWORD},
+                                    format='json')
+        self.assertEqual(response.status_code, 200)
+
+        user_id = json.loads(response.content).get('id')
+
+        response_token = self.client.post(
+            '/api/auth/token/',
+            {
+                'email': MOCK_EMAIL,
+                'password': MOCK_PASSWORD
+            },
+            format='json'
+        )
+
+        jwt_token = json.loads(response_token.content).get('access')
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + jwt_token)
+
+        dompet_data = {
+            "account_title": "Test Account",
+            "user": user_id
+        }
+
+        response_create_dompet = self.client.post(URL_DOMPET, dompet_data)
+
+        self.assertEqual(response_create_dompet.status_code, 201)
+        self.assertEqual(Dompet.objects.count(), 1)
+
+        content = json.loads(response_create_dompet.content)
+        dompet_id = content.get('account_id')
+        self.assertEqual(dompet_data['account_title'], content.get('account_title'))
+
+        response_get_all_dompet = self.client.get(URL_DOMPET)
+
+        content = json.loads(response_get_all_dompet.content)
+        self.assertTrue(content.get('results'))
+        self.assertEqual(dompet_data['account_title'], content.get('results')[0]['account_title'])
+        self.assertEqual(dompet_data['user'], content.get('results')[0]['user'])
+        self.assertTrue(content.get('results')[0]['created_at'])
+        self.assertTrue(content.get('results')[0]['updated_at'])
+
+        url_one_dompet = URL_DOMPET + dompet_id + '/'
+
+        response_get_one_dompet = self.client.get(url_one_dompet)
+
+        content = json.loads(response_get_one_dompet.content)
+        self.assertEqual(dompet_id, content.get('account_id'))
+        self.assertEqual(dompet_data['account_title'], content.get('account_title'))
+        self.assertEqual(dompet_data['user'], content.get('user'))
+        self.assertEqual(user_id, content.get('user'))
+        self.assertTrue(content.get('created_at'))
+        self.assertTrue(content.get('updated_at'))
+
+        dompet_updated_data = {
+            "account_title": "Test Account Updated",
+            "user": user_id
+        }
+
+        response_update_dompet = self.client.put(url_one_dompet, dompet_updated_data)
+
+        content = json.loads(response_update_dompet.content)
+
+        response_get_one_dompet = self.client.get(url_one_dompet)
+
+        content = json.loads(response_get_one_dompet.content)
+        self.assertEqual(dompet_id, content.get('account_id'))
+        self.assertEqual(dompet_updated_data['account_title'], content.get('account_title'))
+        self.assertEqual(dompet_updated_data['user'], content.get('user'))
+        self.assertEqual(user_id, content.get('user'))
+        self.assertTrue(content.get('created_at'))
+        self.assertTrue(content.get('updated_at'))
+
+        self.client.delete(url_one_dompet)
+
+        self.assertEqual(Dompet.objects.count(), 0)
