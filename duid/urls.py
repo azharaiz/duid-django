28a62a88476eeb183863cc30778c8d0adfd2cd94
Azhar Rais <dev@azharaiz.com>
2020-12-18 09:30:55 +0700

    [RED] Test create user view

--- duid/urls.py
+++ duid/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('api/dompet/', include('dompet.urls'), name='dompet_base'),
     path('api/auth/', include(('authentication.urls', 'authentication'), namespace='duid_auth')),
-    path('api/category/', include('category.urls', namespace='category'))
+    path('api/category/', include('category.urls', namespace='category')),
+    path('api/user/', include(('user.urls', 'user'), namespace='duid_user'))
 ]
